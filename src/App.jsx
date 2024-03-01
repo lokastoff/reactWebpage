53359738705f96e3f15a9a3d947bce59bbfd5c0c
Egor Loakstov <mrlokast@gmail.com>
2024-03-01 22:32:22 +0300

    placeholder for patchrelease

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Products } from "./pages/products/Products"
 import { PatchRelease } from "./pages/patchrelease/Patchrelease"
 import { Auth } from "./pages/auth/Auth"
 import { Myacc } from "./pages/accountinfo/Myacc"
+import { Placeholder } from "./components/placeholder/Palceholder"
 function App() {
 
   return (
@@ -12,7 +13,7 @@ function App() {
       <Route path='/' element={<Home/>}></Route>
       <Route path ='/products' element={<Products/>}></Route>
       <Route path='/pricing' element={<Pricing/>}></Route>
-      <Route path='/patchRelease' element={<PatchRelease/>}></Route>
+      <Route path='/patchRelease' element={<Placeholder/>}></Route>
       <Route path='/auth' element={<Auth/>}></Route>
       <Route path='/personalinfo' element={<Myacc/>}></Route>
     </Routes>
