53839635f260750659003307b6659ffc0d1c625f
Egor Loakstov <mrlokast@gmail.com>
2024-03-01 22:39:18 +0300

    placeholder for patch release (got no proper design template)

--- src/components/placeholder/Palceholder.jsx
+++ src/components/placeholder/Palceholder.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { HeaderNoAnim } from '../header/HeaderNoAnim'
+import { Footer } from '../footer/Footer'
 export const Placeholder = () =>{
     return(
-        <section className='flex justify-center items-center text-center h-screen bg-headerImg bg-cover bg-center'>
-         <motion.p  initial ={{opacity:0}} animate={{opacity:1}} transition= {{duration:1.3, delay:0.25}} className='text-white'>Page is empty</motion.p>
-        </section>
+        <div className='pageWrapper static'>
+            <HeaderNoAnim/>
+            <section className='flex justify-center items-center text-center h-screen bg-headerImg bg-cover bg-center'>
+            <motion.p  initial ={{opacity:0}} animate={{opacity:1}} transition= {{duration:1.3, delay:0.25}} className='text-white'>Page is empty</motion.p>
+            </section>
+            <Footer/>
+        </div>
     )
 }
